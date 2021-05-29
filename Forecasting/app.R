01619adaafb664bc673918e01d398da3721dc3b8
hanggaa <hanggaprihadi@gmail.com>
2021-05-29 12:48:14 +0700

    Reset

--- Forecasting/app.R
+++ Forecasting/app.R
@@ -17,19 +17,13 @@ ui <- dashboardPage(
             # First tab content
             tabItem(tabName = "dashboard",
                     fluidRow(
-                        box(title = "Forecast Rice Price", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot1", height = 215),
-                            verbatimTextOutput("detail")
-                            ),
-                        box(title = "Forecast Rice Produce", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot2", height = 215),
-                            verbatimTextOutput("detail2")
-                            ),
+                        box(title = "Forecast Rice Price", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot1", height = 215)),
+                        box(title = "Forecast Rice Produce", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot2", height = 215)),
                         box(
                             title = "Controls", status = "danger", solidHeader = TRUE, collapsible = TRUE, 
                             sliderInput("slider", "Number of observations:", 1, 100, 50),
                             textInput("text", "Text Input:")
-                        ),
-                        box(
-                            title = "Blabla", status = "warning", solidHeader = TRUE, collapsible = TRUE
+                            
                         )
                     )
             ),
@@ -50,23 +44,18 @@ ui <- dashboardPage(
 server <- function(input, output) {
     hangga = read.csv("D:/supply_beras.csv",sep = ",")
     head(hangga)
-    cb <- hangga$Karawang
+    cb <- hangga$Jatim
     supply <- ts(cb, start (2012,1), frequency = 12)
     plot(supply)
     library(forecast)
     library(nnfor)
-    #HW
     hw <- HoltWinters(supply)
     plot(hw)
     f1 <- forecast(hw, h=90)
-    detail2 <- accuracy(f1)
-    #MLP
     ml <- mlp(supply)
     plot(ml)
     f2 = forecast(ml, h=90)
-    detail <- accuracy(f2)
-    output$detail <- renderText({ detail[,"MAPE"] })
-    output$detail2 <- renderText({ detail2[,"MAPE"]})
+    summary(f2)
     output$plot1 <- renderPlot({
         plot(f1)
     })
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Forecasting-Using-R-Shiny
+
+Lorem Ipsum Dolor Sit Amet
+
+# About Author
+Team Bangkit Cap-0123
+1. Hangga
+2. Dede
+3. Salsafira
+4. Tasya 
