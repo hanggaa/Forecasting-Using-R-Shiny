1ba208ba767690a23cf6a93d5c420e5b501ac071
dedeherman <dedehermanvpay@gmail.com>
2021-05-29 12:55:56 +0700

    Nambah button dan widget

--- Forecasting/app.R
+++ Forecasting/app.R
@@ -2,36 +2,32 @@
 library(shinydashboard)
 
 ui <- dashboardPage(
-    header <- dashboardHeader(title = "Forecasting"),
+    header <- dashboardHeader(title = "O.S Forecasting"),
     
     ## Sidebar content
     sidebar <- dashboardSidebar(
         sidebarMenu(
-            menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-            menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+            menuItem("Rice Production", tabName = "dashboard1", icon = icon("dashboard")),
+            menuItem("Rice Price", tabName = "dashboard2", icon = icon("dashboard"))
         )
     ),
     ## Body content
     body <- dashboardBody(
         tabItems(
-            # First tab content
-            tabItem(tabName = "dashboard",
+            # First Tab Content
+            tabItem(tabName = "dashboard1",
                     fluidRow(
-                        box(title = "Forecast Rice Price", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot1", height = 215)),
-                        box(title = "Forecast Rice Produce", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot2", height = 215)),
-                        box(
-                            title = "Controls", status = "danger", solidHeader = TRUE, collapsible = TRUE, 
-                            sliderInput("slider", "Number of observations:", 1, 100, 50),
-                            textInput("text", "Text Input:")
-                            
+                        box(title = "Forecast Rice Produce Holt-Winter Method", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot1", height = 200),
+                            verbatimTextOutput("detail")
+                            ),
+                        box(title = "Forecast Rice Produce MLP Method", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot2", height = 200),
+                            verbatimTextOutput("detail2")
+                            ),
+                        box(title = "Forecast Rice Produce Auto-Arima Method", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot3", height = 200),
+                            verbatimTextOutput("detail3")
                         )
                     )
             ),
-            
-            # Second tab content
-            tabItem(tabName = "widgets",
-                    h2("Widgets tab content")
-            )
         )
     ),
     dashboardPage(
@@ -42,25 +38,31 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output) {
+    #Rice Produce
     hangga = read.csv("D:/supply_beras.csv",sep = ",")
-    head(hangga)
-    cb <- hangga$Jatim
+    cb <- hangga$Karawang
     supply <- ts(cb, start (2012,1), frequency = 12)
     plot(supply)
     library(forecast)
     library(nnfor)
+    #HW Produce
     hw <- HoltWinters(supply)
     plot(hw)
-    f1 <- forecast(hw, h=90)
+    fhwp <- forecast(hw, h=90)
+    detail <- accuracy(fhwp)
+    #MLP Produce
     ml <- mlp(supply)
     plot(ml)
-    f2 = forecast(ml, h=90)
-    summary(f2)
+    fmlpp = forecast(ml, h=90)
+    detail2 <- accuracy(fmlpp)
+    
+    output$detail <- renderText({ detail[,"MAPE"] })
+    output$detail2 <- renderText({ detail2[,"MAPE"]})
     output$plot1 <- renderPlot({
-        plot(f1)
+        plot(fhwp)
     })
     output$plot2 <- renderPlot({
-        plot(f2)
+        plot(fmlpp)
     })
 }
 
--- README.md
+++ README.md
@@ -1,10 +1,1 @@
 # Forecasting-Using-R-Shiny
-
-Lorem Ipsum Dolor Sit Amet
-
-# About Author
-Team Bangkit Cap-0123
-1. Hangga
-2. Dede
-3. Salsafira
-4. Tasya 
